11491dee456483358bc76083f3c550895d1f1cce
zeroincombenze <info@shs-av.com>
2022-06-17 18:20:34 +0200

    [IMP] Refactoring

--- profile_base/__manifest__.py
+++ profile_base/__manifest__.py
@@ -2,27 +2,24 @@
 #
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 #
-
 {
     "name": "Profilo di base",
-    "summary": "Profilo di base per tutte le installazioni",
     "version": "12.0.1.0.1",
     "category": "Localization/Account Charts",
+    "summary": "Profilo di base per tutte le installazioni",
+    "author": "SHS-AV s.r.l.",
+    "website": "https://www.zeroincombenze.it",
+    "development_status": "Alpha",
+    "license": "AGPL-3",
     "depends": [
         "base",
-        "remove_odoo_enterprise",  # OCA server-brand
+        "remove_odoo_enterprise",
         "disable_odoo_online",
         "partner_data_from_vies",
-        "l10n_it_fiscalcode",  # OCA l10n_italy
-        "report_xlsx",  # OCA reporting-engine
+        "l10n_it_fiscalcode",
+        "report_xlsx",
         "repository_check",
     ],
-    "author": """
-        Powerp
-    """,
-    "website": "https://github.com/OCA/l10n-italy",
-    "license": "AGPL-3",
-    "data": [],
     "installable": True,
     "application": False,
     "auto_install": False,
--- profile_common/__manifest__.py
+++ profile_common/__manifest__.py
@@ -1,29 +1,25 @@
 # Copyright 2018-22 SHS-AV s.r.l. <https://www.zeroincombenze.it>
 #
-# Contributions to development, thanks to:
-# * Antonio Maria Vigliotti <antoniomaria.vigliotti@gmail.com>
-#
-# Copyright 2022 LibrERP enterprise network <https://www.librerp.it>
-#
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 #
 {
     "name": "profile common",
-    "summary": "Common setup for all profiles",
-    "version": "12.0.0.1.1",
+    "version": "12.0.0.1.2",
     "category": "Technical Settings",
+    "summary": "Common setup for all profiles",
     "author": "SHS-AV s.r.l.",
-    "website": "https://github.com/OCA/l10n-italy",
+    "website": "https://www.zeroincombenze.it",
+    "development_status": "Beta",
+    "license": "LGPL-3",
     "depends": [
         "account",
         "account_cancel",
         "purchase",
         "sale",
-        "sale_management",  # Only 12.0
+        'sale_management',                    # Only 12.0
         "stock",
     ],
     "data": ["data/initial_conf.xml"],
+    "maintainer": "Antonio M. Vigliotti <antoniomaria.vigliotti@gmail.com>",
     "installable": True,
-    "maintainer": "Antonio Maria Vigliotti",
-    "development_status": "Beta",
 }
